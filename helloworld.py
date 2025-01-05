6d54321701e04c3bdb7ce56f50e4ee392aa60417
ellebelle99 <andreasellegaard123@gmail.com>
2025-01-05 22:06:26 +0100

    1

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# HEADER SECTION
+st.subheader("Hi, I am Andreas :wave:")
+st.title("My very first webapp")
+st.write("I am just trying to get shit to work")
+st.write("[Learn More >](https://www.google.dk)")
